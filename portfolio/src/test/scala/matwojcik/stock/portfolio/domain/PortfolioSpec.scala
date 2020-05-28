ba5b5d6b869ecbc0f36df0943884fe4e051f0202
matwojcik <mat.wojcik@gmail.com>
2020-05-28 21:02:47 +0200

    Refactored tests a bit

--- portfolio/src/test/scala/matwojcik/stock/portfolio/domain/PortfolioSpec.scala
+++ portfolio/src/test/scala/matwojcik/stock/portfolio/domain/PortfolioSpec.scala
@@ -3,12 +3,12 @@ package matwojcik.stock.portfolio.domain
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
+import cats.scalatest.EitherValues
 import matwojcik.stock.domain.Stock
 import matwojcik.stock.domain.Stock.Quantity
 import matwojcik.stock.portfolio.domain.Portfolio.failures.NotEnoughBalance
 import org.scalatest.featurespec.AnyFeatureSpec
 import org.scalatest.matchers.should.Matchers
-import org.scalatest.EitherValues
 import org.scalatest.GivenWhenThen
 
 class PortfolioSpec extends AnyFeatureSpec with Matchers with GivenWhenThen with EitherValues {
@@ -19,39 +19,41 @@ class PortfolioSpec extends AnyFeatureSpec with Matchers with GivenWhenThen with
     Scenario("Empty portfolio") {
       val portfolio = Portfolio.empty
 
-      When("New transaction is added")
+      When("New buy transaction is added")
       val transaction = Transaction(Transaction.Id("1"), Stock.Id("OCDO"), Transaction.Type.Buy, Stock.Quantity(10), someDate)
       val newPortfolio = portfolio.addTransaction(transaction)
 
       Then("Portfolio should have single holding with that stock")
-      newPortfolio.right.value.activeHoldings should contain only (Holding(transaction.stock, transaction.quantity))
+      newPortfolio.value.activeHoldings should contain only (Holding(transaction.stock, transaction.quantity))
     }
 
-    Scenario("Portfolio having this stock") {
+    Scenario("Existing holding") {
+      When("Portfolio contains holding for stock")
       val transaction1 = Transaction(Transaction.Id("1"), Stock.Id("OCDO"), Transaction.Type.Buy, Stock.Quantity(10), someDate)
       val portfolio = Portfolio.empty.addTransaction(transaction1)
 
-      When("New transaction of the same stock is added")
+      When("New buy transaction of the same stock is added")
       val transaction2 = Transaction(Transaction.Id("1"), Stock.Id("OCDO"), Transaction.Type.Buy, Stock.Quantity(5), someDate)
-      val newPortfolio = portfolio.right.value.addTransaction(transaction2)
+      val newPortfolio = portfolio.value.addTransaction(transaction2)
 
-      Then("Portfolio should have single holding with that stock")
-      newPortfolio.right.value.activeHoldings should contain only (Holding(
+      Then("Portfolio should have single holding with that stock with increased quantity")
+      newPortfolio.value.activeHoldings should contain only (Holding(
         transaction1.stock,
         transaction1.quantity plus transaction2.quantity
       ))
     }
 
-    Scenario("Portfolio having other stock") {
+    Scenario("New holding") {
+      When("Portfolio has some holding")
       val transaction1 = Transaction(Transaction.Id("2"), Stock.Id("CDP"), Transaction.Type.Buy, Stock.Quantity(10), someDate)
       val portfolio = Portfolio.empty.addTransaction(transaction1)
 
-      When("New transaction of the same stock is added")
+      When("New buy transaction of different stock is added")
       val transaction2 = Transaction(Transaction.Id("1"), Stock.Id("OCDO"), Transaction.Type.Buy, Stock.Quantity(5), someDate)
-      val newPortfolio = portfolio.right.value.addTransaction(transaction2)
+      val newPortfolio = portfolio.value.addTransaction(transaction2)
 
-      Then("Portfolio should have single holding with that stock")
-      newPortfolio.right.value.activeHoldings should contain only (Holding(transaction1.stock, transaction1.quantity), Holding(
+      Then("Portfolio should have two holdings")
+      newPortfolio.value.activeHoldings should contain only (Holding(transaction1.stock, transaction1.quantity), Holding(
         transaction2.stock,
         transaction2.quantity
       ))
@@ -60,50 +62,54 @@ class PortfolioSpec extends AnyFeatureSpec with Matchers with GivenWhenThen with
 
   Feature("Adding sell transaction") {
     Scenario("No such stock") {
+      Given("Portfolio is empty")
       val portfolio = Portfolio.empty
 
-      When("New transaction is added")
+      When("New sell transaction is added")
       val transaction = Transaction(Transaction.Id("1"), Stock.Id("OCDO"), Transaction.Type.Sell, Stock.Quantity(10), someDate)
       val result = portfolio.addTransaction(transaction)
 
-      Then("Portfolio should not be changed")
-      result.left.value shouldBe (NotEnoughBalance(transaction.stock))
+      Then("Portfolio should not be changed returning failure instead")
+      result.leftValue shouldBe (NotEnoughBalance(transaction.stock))
     }
 
     Scenario("Not enough stock") {
+      When("Portfolio has some holding")
       val transaction1 = Transaction(Transaction.Id("1"), Stock.Id("OCDO"), Transaction.Type.Buy, Stock.Quantity(5), someDate)
       val portfolio = Portfolio.empty.addTransaction(transaction1)
 
-      When("New transaction is added")
+      When("New sell transaction is added which exceeds holding")
       val transaction2 = Transaction(Transaction.Id("1"), Stock.Id("OCDO"), Transaction.Type.Sell, Stock.Quantity(10), someDate)
-      val result = portfolio.right.value.addTransaction(transaction2)
+      val result = portfolio.value.addTransaction(transaction2)
 
-      Then("Portfolio should not be changed")
-      result.left.value shouldBe (NotEnoughBalance(transaction2.stock))
+      Then("Portfolio should not be changed returning failure instead")
+      result.leftValue shouldBe (NotEnoughBalance(transaction2.stock))
     }
 
     Scenario("Just enough stock") {
+      When("Portfolio has some holding")
       val transaction1 = Transaction(Transaction.Id("1"), Stock.Id("OCDO"), Transaction.Type.Buy, Stock.Quantity(5), someDate)
       val portfolio = Portfolio.empty.addTransaction(transaction1)
 
-      When("New transaction is added")
+      When("New sell transaction is added which equals holding")
       val transaction2 = Transaction(Transaction.Id("1"), Stock.Id("OCDO"), Transaction.Type.Sell, Stock.Quantity(5), someDate)
-      val result = portfolio.right.value.addTransaction(transaction2)
+      val result = portfolio.value.addTransaction(transaction2)
 
-      Then("Portfolio should be empty")
-      result.right.value.activeHoldings shouldBe empty
+      Then("Holding should be removed from portfolio")
+      result.value.activeHoldings shouldBe empty
     }
 
     Scenario("More than enough stock") {
+      When("Portfolio has some holding")
       val transaction1 = Transaction(Transaction.Id("1"), Stock.Id("OCDO"), Transaction.Type.Buy, Stock.Quantity(6), someDate)
       val portfolio = Portfolio.empty.addTransaction(transaction1)
 
-      When("New transaction is added")
+      When("New sell transaction is added which is less than holding")
       val transaction2 = Transaction(Transaction.Id("1"), Stock.Id("OCDO"), Transaction.Type.Sell, Stock.Quantity(5), someDate)
-      val result = portfolio.right.value.addTransaction(transaction2)
+      val result = portfolio.value.addTransaction(transaction2)
 
-      Then("Portfolio should be empty")
-      result.right.value.activeHoldings should contain only Holding(transaction1.stock, Quantity(1))
+      Then("Holding should be adjusted")
+      result.value.activeHoldings should contain only Holding(transaction1.stock, Quantity(1))
     }
   }
 
